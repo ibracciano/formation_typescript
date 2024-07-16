08eb029e3b0f41d1fa96d33374246b0a8f5cde0f
ouattara <ouattaraibrahimyacouba@gmail.com>
2024-07-16 15:35:20 +0000

    modification du cours sur les héritages

--- 13.Heritage/app.js
+++ 13.Heritage/app.js
@@ -1,15 +1,42 @@
 "use strict";
-class Person {
-    constructor(name, age) {
+class SuperClass {
+    // proprietes et methodes de la classe
+    constructor() {
+        // initialisation de la class
+    }
+}
+class SubClass extends SuperClass {
+    // proprietes et methodes de la sous-classe
+    constructor() {
+        super(); // appel du constructeur de la super-classe
+        // initialisation de la sous-classe
+    }
+    // Methodes propres à la sous-classe
+    methodSubClass() {
+        // console.log("Methode de la sous-classe");
+    }
+}
+// Exercice pratique
+class User {
+    constructor(id, name, age) {
+        this.id = id;
         this.name = name;
         this.age = age;
     }
-    greet() {
-        console.log(`Bonjour, je m'appelle ${this.name} et j'ai ${this.age} ans`);
+    getInfos() {
+        return `L'utilisateur numero ${this.id}, se nomme ${this.name}. Il a ${this.age} ans`;
     }
 }
-// instancier une class
-let person1 = new Person("John Doe", 30);
-console.log(person1.greet());
-let person2 = new Person("Ouattara Ibrahim", 28);
-console.log(person2.greet());
+class Employee extends User {
+    constructor(id, name, age, department) {
+        super(id, name, age);
+        this.department = department;
+    }
+    getInfos() {
+        return `${super.getInfos()} et travaille dans le departement ${this.department}`;
+    }
+}
+const employee1 = new Employee(1, "John", 29, "Assistance sociale");
+const employee2 = new Employee(2, "Doe", 20, "Comptabilité");
+console.log(employee1.getInfos()); // L'utilisateur  numero 1 se nomme John et a 29 ans, travaille dans le departement Assistance sociale
+console.log(employee2.getInfos()); // L'utilisateur  numero 1 se nomme John et a 29 ans, travaille dans le departement Assistance sociale
--- 13.Heritage/app.ts
+++ 13.Heritage/app.ts
@@ -17,3 +17,41 @@ class SubClass extends SuperClass {
     // console.log("Methode de la sous-classe");
   }
 }
+
+// Exercice pratique
+
+class User {
+  id: number;
+  name: string;
+  age: number;
+
+  constructor(id: number, name: string, age: number) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  getInfos(): string {
+    return `L'utilisateur numero ${this.id}, se nomme ${this.name}. Il a ${this.age} ans`;
+  }
+}
+
+class Employee extends User {
+  department: string;
+
+  constructor(id: number, name: string, age: number, department: string) {
+    super(id, name, age);
+    this.department = department;
+  }
+  getInfos(): string {
+    return `${super.getInfos()} et travaille dans le departement ${
+      this.department
+    }`;
+  }
+}
+
+const employee1 = new Employee(1, "John", 29, "Assistance sociale");
+const employee2 = new Employee(2, "Doe", 20, "Comptabilité");
+
+console.log(employee1.getInfos()); // L'utilisateur  numero 1 se nomme John et a 29 ans, travaille dans le departement Assistance sociale
+console.log(employee2.getInfos()); // L'utilisateur  numero 1 se nomme John et a 29 ans, travaille dans le departement Assistance sociale
